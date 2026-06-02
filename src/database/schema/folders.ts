ed352f58d7a1a61bf8ef92051bf3da6687d227cc
HeyVenom49 <durgeshpandeysmart@gmail.com>
2026-06-03 02:24:15 +0530

    feat: add folders schema to database with necessary fields and relationships

---
+++ src/database/schema/folders.ts
@@ -0,0 +1,18 @@
+import { boolean, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { users } from "./users.ts";
+
+export const folders = pgTable("folders", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+  ownerId: uuid("owner_id")
+    .notNull()
+    .references((): any => folders.id, {
+      onDelete: "cascade",
+    }),
+  parentId: uuid("parent_id").references((): any => folders.id, {
+    onDelete: "cascade",
+  }),
+  isDeleted: boolean("is_deleted").default(false).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
