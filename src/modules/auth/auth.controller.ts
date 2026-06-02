9fa811f227a75380f4b4ac575e0de968b27c3b79
HeyVenom49 <durgeshpandeysmart@gmail.com>
2026-06-02 15:28:29 +0530

    feat: implement login functionality with validation and user authentication

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import { registerSchema } from "./auth.schema.ts";
+import { loginSchema, registerSchema } from "./auth.schema.ts";
 import { authService } from "./auth.service.ts";
 
 export class AuthController {
@@ -7,6 +7,17 @@ export class AuthController {
     const data = registerSchema.parse(req.body);
 
     const user = await authService.register(data);
+
+    return res.status(201).json({
+      success: true,
+      data: user,
+    });
+  }
+
+  async login(req: Request, res: Response) {
+    const data = loginSchema.parse(req.body);
+
+    const user = await authService.login(data);
 
     return res.status(201).json({
       success: true,
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import argon2 from "argon2";
 import { userRepository } from "../users/user.repository.ts";
-import type { RegisterInput } from "./auth.schema.ts";
+import type { LoginInput, RegisterInput } from "./auth.schema.ts";
 
 export class AuthService {
   async register(data: RegisterInput) {
@@ -23,6 +23,25 @@ export class AuthService {
       username: user?.username,
       email: user?.email,
       createdAt: user?.created_at,
+    };
+  }
+
+  async login(data: LoginInput) {
+    const user = await userRepository.findByEmail(data.email);
+    if (!user) {
+      throw new Error("Email or Password might be wrong");
+    }
+    const isPasswordCorrect = argon2.verify(user.password_hash, data.password);
+
+    if (!isPasswordCorrect) {
+      throw new Error("Email or Password might be wrong");
+    }
+
+    return {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      createdAt: user.created_at,
     };
   }
 }
