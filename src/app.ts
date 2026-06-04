76b90c710b5eec9fb3d4bc31d736a52645d9bb38
HeyVenom49 <durgeshpandeysmart@gmail.com>
2026-06-04 23:51:12 +0530

    refactor: remove error handling middleware and related imports

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { errorMiddleware } from "./common/middleware/error.middleware.ts";
 import routes from "./routes/index.ts";
 
 const app = express();
@@ -7,7 +6,5 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
-
-app.use(errorMiddleware);
 
 export default app;
